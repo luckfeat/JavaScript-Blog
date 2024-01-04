82591b777d1b714f5c7583087df0e1b458cd970f
luckfeat <luckfeat@gmail.com>
2024-01-04 10:54:14 +0900

    test

--- src/core/api/firebase.js
+++ src/core/api/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyBauxDyK7Wjp6IZF__tLd47cLIQsTArVNQ',
   authDomain: 'javascript-blog-service.firebaseapp.com',
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: '1:133218015549:web:d87b274121fab024a94d54',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
