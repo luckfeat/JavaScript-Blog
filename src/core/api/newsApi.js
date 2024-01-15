c02d61b47e982e6dff60e5d5f8f9d47efd367164
luckfeat <luckfeat@gmail.com>
2024-01-16 00:06:05 +0900

    refactor: Update fetchArticles function

--- src/core/api/newsApi.js
+++ src/core/api/newsApi.js
@@ -6,9 +6,11 @@ export default async function fetchArticles() {
   const category = 'technology';
   const max = 25;
   const requestUrl = `${baseUrl}?category=${category}&lang=en&country=us&expand=content&max=${max}&apikey=${apiKey}`;
-
-  let response = await fetch(requestUrl);
-  let data = await response.json();
-
-  return data;
+  try {
+    const response = await fetch(requestUrl);
+    return await response.json();
+  } catch (error) {
+    console.error('기사를 불러오는 데 실패했습니다.', error);
+    throw error;
+  }
 }
