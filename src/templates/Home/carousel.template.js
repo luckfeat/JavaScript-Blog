d16332bb3b67440697d8a468eb49d34d9408a8f4
luckfeat <luckfeat@gmail.com>
2024-01-15 15:02:58 +0900

    feat: Add carousel template

--- src/templates/Home/carousel.template.js
+++ src/templates/Home/carousel.template.js
@@ -1,5 +1,10 @@
 const template = `
-    <h1>Carousel</h1>
+    <h3>Main</h3>
+    <ul class="carousel">
+        {{#each articles}}
+            <li>{{this}}</li>
+        {{/each}}
+    </ul>
 `;
 
 export default window.Handlebars.compile(template);
