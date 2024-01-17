6eb201c92b58e4ab6417399ceb20025a8d5b0357
luckfeat <luckfeat@gmail.com>
2024-01-17 14:10:41 +0900

    feat: Create keyword, daily, writer, recommend components

---
+++ src/components/Home/Daily.js
@@ -0,0 +1,8 @@
+import template from '../../templates/Home/daily.template';
+import Component from '../../core/component';
+
+export default class Daily extends Component {
+    constructor(data) {
+        super(template, data);
+    }
+}
---
+++ src/components/Home/Keyword.js
@@ -0,0 +1,8 @@
+import template from '../../templates/Home/keyword.template';
+import Component from '../../core/component';
+
+export default class Keyword extends Component {
+    constructor(data) {
+        super(template, data);
+    }
+}
