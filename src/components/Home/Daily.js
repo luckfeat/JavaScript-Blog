495c661b1fb04430668a5d57b3b434756bb17889
luckfeat <luckfeat@gmail.com>
2024-01-18 16:04:01 +0900

    feat: Add eslint

--- src/components/Home/Daily.js
+++ src/components/Home/Daily.js
@@ -2,7 +2,7 @@ import template from '../../templates/Home/daily.template';
 import Component from '../../core/component';
 
 export default class Daily extends Component {
-    constructor(data) {
-        super(template, data);
-    }
+  constructor(data) {
+    super(template, data);
+  }
 }
