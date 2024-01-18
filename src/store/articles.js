cffccd095c66a76df3ca08f6c2efa0f30b291835
luckfeat <luckfeat@gmail.com>
2024-01-18 15:13:08 +0900

    fix

--- src/store/articles.js
+++ src/store/articles.js
@@ -2,29 +2,29 @@ import Store from '../core/store';
 import fetchArticles from '../core/api/newsApi';
 
 const store = new Store({
-    articles: [],
-    page: 1
+  articles: [],
+  page: 1,
 });
 
 export async function loadArticles() {
-    try {
-        let {articles} = await fetchArticles();
-        store.state.articles = articles
-        return articles
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const { articles } = await fetchArticles();
+    store.state.articles = articles;
+    return articles;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function loadNextArticles() {
-    try {
-        store.state.page++
-        let {articles} = await fetchArticles(true);
-        store.state.articles = articles
-        return articles
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    store.state.page++;
+    const { articles } = await fetchArticles(true);
+    store.state.articles = articles;
+    return articles;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export default store;
