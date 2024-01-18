3d5ebf6f2149955ec3986baaf8ca360839df0a81
luckfeat <luckfeat@gmail.com>
2024-01-18 22:23:47 +0900

    fix: Remove dependency cycle

--- src/core/api/newsApi.js
+++ src/core/api/newsApi.js
@@ -1,26 +1,25 @@
-import config from '../../../config.mjs';
-import articlesStore from '../../store/articles.js'
+import config from '../../../config';
 
-export default async function fetchArticles(increment = false, search = false, keyword) {
-    const baseUrl = 'https://gnews.io/api/v4';
-    const apiKey = config.apiKey;
-    const category = 'technology';
-    const max = 25;
-    const page = increment ? articlesStore.state.page : 1
+export default async function fetchArticles(search = false, keyword = '', increment = false, pageNumber = 1) {
+  const baseUrl = 'https://gnews.io/api/v4';
+  const { apiKey } = config;
+  const category = 'technology';
+  const max = 25;
+  const page = increment ? pageNumber : 1;
 
-    const requestUrl = {url: ''};
+  const requestUrl = { url: '' };
 
-    if (!search) {
-        requestUrl.url = `${baseUrl}/top-headlines?category=${category}&lang=en&country=us&expand=content&max=${max}&page=${page}&apikey=${apiKey}`;
-    } else {
-        requestUrl.url = `${baseUrl}/search?q=${keyword}&apikey=${apiKey}`;
-    }
+  if (!search) {
+    requestUrl.url = `${baseUrl}/top-headlines?category=${category}&lang=en&country=us&expand=content&max=${max}&page=${page}&apikey=${apiKey}`;
+  } else {
+    requestUrl.url = `${baseUrl}/search?q=${keyword}&lang=en&country=us&expand=content&max=${max}&apikey=${apiKey}`;
+  }
 
-    try {
-        const response = await fetch(requestUrl.url);
-        return await response.json();
-    } catch (error) {
-        console.error('기사를 불러오는 데 실패했습니다.', error);
-        throw error;
-    }
+  try {
+    const response = await fetch(requestUrl.url);
+    return await response.json();
+  } catch (error) {
+    console.error('기사를 불러오는 데 실패했습니다.', error);
+    throw error;
+  }
 }
