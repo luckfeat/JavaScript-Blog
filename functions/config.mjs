233c3a2fcee68f532f11b0d28c3a208d2e422cb4
luckfeat <luckfeat@gmail.com>
2024-02-06 18:56:22 +0900

    find error in sendRequestInBatches

--- functions/config.mjs
+++ functions/config.mjs
@@ -3,13 +3,14 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 
 const config = {
-  firebaseKey: process.env.FIREBASE,
+  firebaseKey: process.env.FIREBASEKEY,
   autoDomain: process.env.AUTODOMAIN,
   projectId: process.env.PROJECTID,
   storageBucket: process.env.STORAGEBUCKET,
   messagingSenderId: process.env.MESSAGINGSENDERID,
   appId: process.env.APPID,
   apiKey: process.env.KEY,
+  secondApiKey: process.env.SECONDKEY
 };
 
 export default config;
--- functions/index.js
+++ functions/index.js
@@ -61,7 +61,7 @@ export const postArticles = functions.https.onCall(async (data, context) => {
         });
       });
 
-      await Promise.all(articlePromises);
+      return Promise.all(articlePromises);
     });
 
     return await Promise.all(promises);
@@ -69,3 +69,102 @@ export const postArticles = functions.https.onCall(async (data, context) => {
     throw new functions.https.HttpsError('unknown', 'Something went wrong');
   }
 });
+
+function sendRequestsInBatches() {
+  async function requestArticles(baseUrl, category, apiKey) {
+    const requestUrl = `${baseUrl}/top-headlines?category=${category}&lang=en&country=us&apikey=${apiKey}`;
+    const response = await fetch(requestUrl);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const { articles } = await response.json();
+
+    articles?.forEach(article =>
+      setDoc(doc(db, 'Articles', article.title), {
+        content: article.content,
+        description: article.description,
+        image: article.image,
+        publishedAt: article.publishedAt,
+        source: article.source,
+        title: article.title,
+        url: article.url,
+      }),
+    );
+  }
+
+  const baseUrl = 'https://gnews.io/api/v4';
+  const categories = [
+    'general',
+    'world',
+    'nation',
+    'business',
+    // 'technology',
+    // 'entertainment',
+    // 'sports',
+    // 'science',
+    // 'health',
+  ];
+  const batchSize = 3;
+  const results = [];
+
+  for (let i = 0; i < categories.length; i += batchSize) {
+    const batch = categories.slice(i, i + batchSize);
+    batch.forEach(async category => {
+      try {
+        await requestArticles(baseUrl, category, config.secondApiKey);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+
+  return results;
+}
+
+// async function test() {
+//   const baseUrl = 'https://gnews.io/api/v4';
+//   const { apiKey } = config;
+//   const categories = [
+//     'general',
+//     'world',
+//     'nation',
+//     'business',
+//     'technology',
+//     'entertainment',
+//     'sports',
+//     'science',
+//     'health',
+//   ];
+//
+//   try {
+//     const batchSize = 3; // 각 배치의 크기를 정의
+//     const results = []; // 결과를 저장할 배열 선언
+//
+//     const promises = categories.map(async category => {
+//       const requestUrl = `${baseUrl}/top-headlines?category=${category}&lang=en&country=us&expand=content&apikey=${apiKey}`;
+//       const response = await fetch(requestUrl);
+//       const { articles } = await response.json();
+//
+//       const articlePromises = articles.map(async article => {
+//         await setDoc(doc(db, 'Articles', article.title), {
+//           content: article.content,
+//           description: article.description,
+//           image: article.image,
+//           publishedAt: article.publishedAt,
+//           source: article.source,
+//           title: article.title,
+//           url: article.url,
+//         });
+//       });
+//
+//       return Promise.all(articlePromises);
+//     });
+//
+//     return await Promise.all(promises);
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// }
+sendRequestsInBatches();
